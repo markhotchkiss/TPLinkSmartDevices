49cf1670f809dc1ce37eaa5d9b7b03b4c22abfc8
Mark Hotchkiss <mark.hotchkiss@idevs.uk>
2019-12-10 19:54:34 +0000

    Added Unit Test for Device Discovery

---
+++ TPLinkSmartDevices.Tests/TpLinkTestFixture.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TPLinkSmartDevices.Tests
+{
+    [TestFixture]
+    public class TpLinkTestFixture
+    {
+        [Test]
+        public async Task DiscoverDevices()
+        {
+            var tpLinkDiscovery = new TPLinkSmartDevices.TPLinkDiscovery();
+
+            var devices = await tpLinkDiscovery.Discover(9999, 10000);
+
+            Assert.GreaterOrEqual(devices.Count, 1);
+        }
+    }
+}
